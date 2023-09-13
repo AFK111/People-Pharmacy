7d8e1bc5741bdf8a9bfa5a24c5ac73274d796c9c
AFK1 <ahmadtheapat1@gmail.com>
2023-09-13 05:23:28 +0300

    Implement home page initial design

---
+++ src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { } from '@fortawesome/fontawesome-common-types';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent {
+
+}
